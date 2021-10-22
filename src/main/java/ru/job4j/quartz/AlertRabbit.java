25a70c6f20987a4ed148ad9f07d02d731510b115
Andrey Laktyushin <andrey@laktyushin.com>
2021-10-22 13:39:31 +0300

    1. Quartz [#175122 #219017]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -5,11 +5,6 @@ import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.*;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -18,23 +13,13 @@ import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
 
-    private Connection connection;
-    private final DateTimeFormatter formatter =
-            DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
-
     public static void main(String[] args) {
         AlertRabbit rabbit = new AlertRabbit();
         int interval = rabbit.init();
         try {
-            List<Long> store = new ArrayList<>();
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDataMap data = new JobDataMap();
-            data.put("store", store);
-            data.put("connection", rabbit.connection);
-            JobDetail job = newJob(Rabbit.class)
-                    .usingJobData(data)
-                    .build();
+            JobDetail job = newJob(Rabbit.class).build();
             SimpleScheduleBuilder times = simpleSchedule()
                     .withIntervalInSeconds(interval)
                     .repeatForever();
@@ -43,27 +28,15 @@ public class AlertRabbit {
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-            Thread.sleep(10000);
-            scheduler.shutdown();
-            System.out.println(store);
-            System.out.println("Thread stopped at " + LocalDateTime.now().format(rabbit.formatter));
-            rabbit.pushTimeWhenJobClosed();
-        } catch (Exception se) {
+        } catch (SchedulerException se) {
             se.printStackTrace();
         }
     }
 
     public static class Rabbit implements Job {
-
-        public Rabbit() {
-            System.out.println(hashCode());
-        }
-
         @Override
         public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
-            List<Long> store = (List<Long>) context.getJobDetail().getJobDataMap().get("store");
-            store.add(System.currentTimeMillis());
         }
     }
 
@@ -74,25 +47,9 @@ public class AlertRabbit {
         Properties config = new Properties();
         try {
             config.load(in);
-            Class.forName(config.getProperty("driver-class-name"));
-            connection = DriverManager.getConnection(
-                    config.getProperty("url"),
-                    config.getProperty("username"),
-                    config.getProperty("password")
-            );
-        } catch (IOException | ClassNotFoundException | SQLException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return Integer.parseInt(config.getProperty("rabbit.interval"));
-    }
-
-    private void pushTimeWhenJobClosed() {
-        try (PreparedStatement statement =
-                     connection.prepareStatement("insert into rabbit(created_date) values (?)")) {
-            statement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 }
