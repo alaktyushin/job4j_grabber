aa435f3d4693ea3a5f1b328b8b304064a43b17ab
Andrey Laktyushin <andrey@laktyushin.com>
2021-10-22 17:05:29 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -6,18 +6,37 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
+import java.io.IOException;
+
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
+        SqlRuParse parseForum = new SqlRuParse();
+        String url = "https://www.sql.ru/forum/job-offers";
+        String cssQuery = ".postslisttopic";
+        for (int i = 1; i <= 5; i++) {
+            String urlN = url. concat("/").concat(String.valueOf(i));
+            parseForum.parsePage(urlN, cssQuery);
+            Thread.sleep(1000);
+        }
+    }
+
+    private void parsePage(String url, String cssQuery) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        Elements row = doc.select(cssQuery);
+        SqlRuDateTimeParser pageParser = new SqlRuDateTimeParser();
+        System.out.println(
+                System.lineSeparator()
+                + "Parsing URL " + url
+                        + System.lineSeparator()
+        );
         for (Element td : row) {
             Element href = td.child(0);
             Element parent = td.parent();
             System.out.println(href.attr("href"));
             System.out.println(href.text());
             if (parent != null) {
-                System.out.println(parser.parse(parent.children().get(5).text()));
+                String strDate = parent.children().get(5).text();
+                System.out.println(pageParser.parse(strDate));
             }
         }
     }
