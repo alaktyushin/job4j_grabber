cba85cae89709ebf72a123e80b75953ed5cef172
Andrey Laktyushin <andrey@laktyushin.com>
2021-10-25 08:58:41 +0300

    2.2. Модель данных - Post. [#285211 #219381]

--- src/main/java/ru/job4j/html/Post.java
+++ src/main/java/ru/job4j/html/Post.java
@@ -11,17 +11,12 @@ public class Post {
     private String description;
     private LocalDateTime created;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Post post = (Post) o;
-        return id == post.id && Objects.equals(link, post.link) && Objects.equals(created, post.created);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, link, created);
+    public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
     }
 
     @Override
@@ -33,5 +28,24 @@ public class Post {
                 + ", description='" + description + '\''
                 + ", created=" + created
                 + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id ==
+                post.id
+                && Objects.equals(title, post.title)
+                && Objects.equals(link, post.link)
+                && Objects.equals(created, post.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, link, created);
     }
 }
